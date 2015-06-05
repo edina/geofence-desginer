f53688b377b96b9d0d34cf90c1eb90a634fad3f0
Panos Terzis <P.Terzis@ed.ac.uk>
2015-06-05 10:49:32 +0100

    make app.css to be a relative path

--- app/router.js
+++ app/router.js
@@ -1,6 +1,6 @@
 import Backbone from 'backbone';
 import { MapView } from './views/Map';
-import 'styles/app.css!';
+import './styles/app.css!';
 
 export class GeofenceRouter extends Backbone.Router {
 
